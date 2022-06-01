3a4527584666f6de12145b183724f1b645b0fccb
rohitm4n <55442226+rohitm4n@users.noreply.github.com>
2022-06-01 17:02:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function clik2(){
+    document.body.scrollTop = 500;
+    document.documentElement.scrollTop = 500;
+    document.getElementById('acts2').style.color='rgb(247, 100, 43)';
+        document.getElementById('acts').style.color='white';
+        document.getElementById('acts3').style.color='white';
+}
+   
+function clik(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    document.getElementById('acts').style.color='rgb(247, 100, 43)';
+    document.getElementById('acts2').style.color='white';
+    document.getElementById('acts3').style.color='white';
+    
+
+}
+function clik3(){
+    document.body.scrollTop = 100;
+    document.documentElement.scrollTop = 1100;
+    document.getElementById('acts3').style.color='rgb(247, 100, 43)';
+    document.getElementById('acts2').style.color='white';
+    document.getElementById('acts').style.color='white';
+    
+
+}
+
